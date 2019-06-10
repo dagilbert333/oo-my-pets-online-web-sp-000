fd61b35effcbde90a0d6dc1f77cb50e871ccf706
David Gilbert <dagilbert333@gmail.com>
2019-06-10 04:48:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,12 +29,12 @@ class Owner
     @pets[:fishes] << Fish.new(name)
   end
   
-  def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
   end
   
-  def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
   end
   
 end
